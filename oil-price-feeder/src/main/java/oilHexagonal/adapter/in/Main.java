44414198af6851af302daf1536a3c105f53d9318
Enrique <eph1806@gmail.com>
2025-05-15 23:36:36 +0100

    Implementado Timer task, eliminado el modelo example.

--- oil-price-feeder/src/main/java/oilHexagonal/adapter/in/Main.java
+++ oil-price-feeder/src/main/java/oilHexagonal/adapter/in/Main.java
@@ -8,11 +8,19 @@ import oilHexagonal.application.controller.Controller;
 public class Main {
     public static void main(String[] args) {
         AlphaVantageProvider oilProvider = new AlphaVantageProvider(args[0]);
-        /*
+        OilEventFeeder oilEventFeeder = new OilEventFeeder();
+        Controller controller = new Controller(oilProvider, oilEventFeeder);
+
+        controller.run();
+
+        try{
+            Thread.sleep(Long.MAX_VALUE);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+                /*
         SQLiteOilStore oilStore = new SQLiteOilStore();
         new Controller(oilProvider, oilStore).run();
         */
-        OilEventFeeder oilEventFeeder = new OilEventFeeder();
-        new Controller(oilProvider, oilEventFeeder).run();
     }
 }
